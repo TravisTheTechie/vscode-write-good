f727577ed31bef2eb025480315d1f128792a7e89
Travis Smith <travis@legomaster.net>
2016-02-23 14:38:43 -0500

    Write-Good Linter mostly working.

--- src/extension.ts
+++ src/extension.ts
@@ -1,101 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the necessary extensibility types to use in your code below
-import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+'use strict';
 
-// This method is called when your extension is activated. Activation is
-// controlled by the activation events defined in package.json.
+import { workspace, Disposable, ExtensionContext, TextDocument, languages,
+         Diagnostic, DiagnosticSeverity, Range, Position } from 'vscode';
+let WriteGood = require('write-good');
+
 export function activate(context: ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error).
-    // This line of code will only be executed once when your extension is activated.
-    console.log('Congratulations, your extension "WordCount" is now active!');
 
-    // create a new word counter
-    let wordCounter = new WordCounter();
-    let controller = new WordCounterController(wordCounter);
+    console.log("Write-Good Linter active...");
 
-    // Add to a list of disposables which are disposed when this extension is deactivated.
-    context.subscriptions.push(controller);
-    context.subscriptions.push(wordCounter);
+    context.subscriptions.push(workspace.onDidChangeTextDocument(event => {
+        doLint(event.document);
+    }));
+
+    context.subscriptions.push(workspace.onDidOpenTextDocument(event => {
+        doLint(event);
+    }));
 }
 
-class WordCounter {
-
-    private _statusBarItem: StatusBarItem;
-
-    public updateWordCount() {
-
-        // Create as needed
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        }
-
-        // Get the current text editor
-        let editor = window.activeTextEditor;
-        if (!editor) {
-            this._statusBarItem.hide();
-            return;
-        }
-
-         let doc = editor.document;
-
-        // Only update status if an MarkDown file
-        if (doc.languageId === "markdown") {
-            let wordCount = this._getWordCount(doc);
-
-            // Update the status bar
-            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '$(pencil) 1 Word';
-            this._statusBarItem.show();
-        } else {
-            this._statusBarItem.hide();
-        }
-    }
-
-    public _getWordCount(doc: TextDocument): number {
-
-        let docContent = doc.getText();
-
-        // Parse out unwanted whitespace so the split is accurate
-        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
-        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        let wordCount = 0;
-        if (docContent != "") {
-            wordCount = docContent.split(" ").length;
-        }
-
-        return wordCount;
-    }
-
-    dispose() {
-        this._statusBarItem.dispose();
-    }
+interface Suggestion {
+    index: number,
+    offset: number,
+    reason: string
 }
 
-class WordCounterController {
+function doLint(document: TextDocument) {
+    let diagnosticCollection = languages.createDiagnosticCollection("Write-Good Lints");
+    let diagnostics : Diagnostic[] = [];
+    let lines = document.getText().split(/\r?\n/g);
+    lines.forEach((line, lineCount) => {
+        let suggestions : Suggestion[] = WriteGood(line);
+        suggestions.forEach((suggestion, si) => {
+            let start = new Position(lineCount, suggestion.index);
+            let end = new Position(lineCount, suggestion.index + suggestion.offset);
+            diagnostics.push(new Diagnostic(new Range(start, end), suggestion.reason, DiagnosticSeverity.Warning));
+        });
+    });
 
-    private _wordCounter: WordCounter;
-    private _disposable: Disposable;
-
-    constructor(wordCounter: WordCounter) {
-        this._wordCounter = wordCounter;
-        this._wordCounter.updateWordCount();
-
-        // subscribe to selection change and editor activation events
-        let subscriptions: Disposable[] = [];
-        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
-        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
-
-        // update the counter for the current file
-        this._wordCounter.updateWordCount();
-
-        // create a combined disposable from both event subscriptions
-        this._disposable = Disposable.from(...subscriptions);
-    }
-
-    dispose() {
-        this._disposable.dispose();
-    }
-
-    private _onEvent() {
-        this._wordCounter.updateWordCount();
-    }
+    diagnosticCollection.clear();
+    diagnosticCollection.set(document.uri, diagnostics);
 }
